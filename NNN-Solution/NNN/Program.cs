9f3ffde0b7c6eb1ab322af6bd48ed0163ccfb2fa
marcov-k <marcoaklima@gmail.com>
2025-09-21 19:16:59 -0400

    New branch for testing variable size networks

--- NNN-Solution/NNN/Program.cs
+++ NNN-Solution/NNN/Program.cs
@@ -1,16 +1,12 @@
 ﻿using MathNet.Numerics.Distributions;
 using MathNet.Numerics.LinearAlgebra;
 
-int epochs = 1000000;
-double alpha = 0.0001;
+int epochs = 10000;
+double alpha = 0.01;
 Matrix<double> x = Matrix<double>.Build.Dense(10, 1, new double[] { 1, 3, 7, 17, 21, 44, 56, 71, 88, 100 });
 int[] n = { 1, 3, 3, 1 };
-Matrix<double> w1 = GenerateMatrix(n[1], n[0]);
-Matrix<double> w2 = GenerateMatrix(n[2], n[1]);
-Matrix<double> w3 = GenerateMatrix(n[3], n[2]);
-Matrix<double> b1Def = GenerateMatrix(n[1], 1);
-Matrix<double> b2Def = GenerateMatrix(n[2], 1);
-Matrix<double> b3Def = GenerateMatrix(n[3], 1);
+List<Matrix<double>> wVals = new List<Matrix<double>>();
+List<Matrix<double>> bDefVals = new List<Matrix<double>>();
 Matrix<double> y = Matrix<double>.Build.Dense(10, 1);
 for (int i = 0; i < x.RowCount; i++)
 {
@@ -42,6 +38,11 @@ void CreateNetworkDefaults(int inputs, int hiddenLayers, int hiddenNodes, int ou
         n[i] = hiddenNodes;
     }
     n[n.Length - 1] = outputs;
+    for (int i = 1; i < n.Length; i++)
+    {
+        wVals.Add(GenerateMatrix(n[i], n[i - 1]));
+        bDefVals.Add(GenerateMatrix(n[i], 1));
+    }
 }
 
 List<double> Train()
@@ -53,42 +54,79 @@ List<double> Train()
         var (yHat, cache) = FeedForward(a0);
         double error = Cost(yHat, y, m);
         costs.Add(error);
-        var (dC_dW3, dC_db3, dC_dA2) = BackpropLayer3(yHat, y, m, cache.a2, w3);
-        var (dC_dW2, dC_db2, dC_dA1) = BackpropLayer2(dC_dA2, cache.a1, cache.a2, w2);
-        var (dC_dW1, dC_db1) = BackpropLayer1(dC_dA1, cache.a1, cache.a0, w1);
-        w3 -= alpha * dC_dW3;
-        w2 -= alpha * dC_dW2;
-        w1 -= alpha * dC_dW1;
-        b3Def -= Broadcast(alpha * dC_db3, b3Def.ColumnCount);
-        b2Def -= Broadcast(alpha * dC_db2, b2Def.ColumnCount);
-        b1Def -= Broadcast(alpha * dC_db1, b1Def.ColumnCount);
+        HandleBackprop(yHat, y, m, cache);
         if (i % 20 == 0) Console.WriteLine($"epoch {i}: cost = {error}");
     }
     return costs;
 }
 
-(Matrix<double> dC_dW3, Matrix<double> dC_db3, Matrix<double> dC_dA2) BackpropLayer3(Matrix<double> yHat, Matrix<double> y, int m, Matrix<double> a2, Matrix<double> w3)
+(Matrix<double> yHat, Cache cache) FeedForward(Matrix<double> a0)
 {
-    Matrix<double> a3 = yHat;
-    Matrix<double> dC_dZ3 = (1 / (double)m) * (a3 - y);
-    Matrix<double> dZ3_dW3 = a2;
-    Matrix<double> dC_dW3 = dC_dZ3 * dZ3_dW3.Transpose();
-    Matrix<double> dC_db3 = CalculateBiasC(dC_dZ3);
-    Matrix<double> dZ3_dA2 = w3;
-    Matrix<double> dC_dA2 = w3.Transpose() * dC_dZ3;
-    return (dC_dW3, dC_db3, dC_dA2);
+    Cache cache = new Cache();
+    cache.aVals.Add(a0);
+    Matrix<double> z = wVals[0] * a0;
+    z += Broadcast(bDefVals[0], z.ColumnCount);
+    Matrix<double> a = Sigmoid(z);
+    cache.aVals.Add(a);
+    for (int i = 1; i < n.Length - 1; i++)
+    {
+        z = wVals[i] * a;
+        z += Broadcast(bDefVals[i], z.ColumnCount);
+        a = Sigmoid(z);
+        cache.aVals.Add(a);
+    }
+    Matrix<double> yHat = cache.aVals[cache.aVals.Count() - 1];
+    cache.aVals.RemoveAt(cache.aVals.Count() - 1);
+    return (yHat, cache);
 }
 
-(Matrix<double> dC_dW2, Matrix<double> dC_db2, Matrix<double> dC_dA1) BackpropLayer2(Matrix<double> propagator_dC_dA2, Matrix<double> a1, Matrix<double> a2, Matrix<double> w2)
+void HandleBackprop(Matrix<double> yHat, Matrix<double> y, int m, Cache cache)
 {
-    Matrix<double> dA2_dZ2 = a2.PointwiseMultiply(1 - a2);
-    Matrix<double> dC_dZ2 = propagator_dC_dA2.PointwiseMultiply(dA2_dZ2);
-    Matrix<double> dZ2_dW2 = a1;
-    Matrix<double> dC_dW2 = dC_dZ2 * dZ2_dW2.Transpose();
-    Matrix<double> dC_db2 = CalculateBiasC(dC_dW2);
-    Matrix<double> dZ2_dA1 = w2;
-    Matrix<double> dC_dA1 = dZ2_dA1.Transpose() * dC_dZ2;
-    return (dC_dW2, dC_db2, dC_dA1);
+    List<Matrix<double>> dC_dWs = new List<Matrix<double>>();
+    List<Matrix<double>> dC_dbs = new List<Matrix<double>>();
+    var (dC_dWL, dC_dbL, dC_dAL) = BackpropFinalLayer(yHat, y, m, cache.aVals[cache.aVals.Count() - 1], wVals[wVals.Count() - 1]);
+    dC_dWs.Insert(0, dC_dWL);
+    dC_dbs.Insert(0, dC_dbL);
+    for (int i = cache.aVals.Count() - 1; i > 1; i--)
+    {
+        Console.WriteLine($"a{i - 1}: {cache.aVals[i] - 1}");
+        Console.WriteLine($"a{i}: {cache.aVals[i]}");
+        (dC_dWL, dC_dbL, dC_dAL) = BackpropHiddenLayer(dC_dAL, cache.aVals[i - 1], cache.aVals[i], wVals[i]);
+        dC_dWs.Insert(0, dC_dWL);
+        dC_dbs.Insert(0, dC_dbL);
+    }
+    (dC_dWL, dC_dbL) = BackpropLayer1(dC_dAL, cache.aVals[1], cache.aVals[0], wVals[0]);
+    dC_dWs.Insert(0, dC_dWL);
+    dC_dbs.Insert(0, dC_dbL);
+    for (int i = wVals.Count() - 1; i >= 0; i--)
+    {
+        wVals[i] -= alpha * dC_dWs[i];
+        bDefVals[i] -= Broadcast(alpha * dC_dbs[i], bDefVals[i].ColumnCount);
+    }
+}
+
+(Matrix<double> dC_dWL, Matrix<double> dC_dbL, Matrix<double> dC_dAL1) BackpropFinalLayer(Matrix<double> yHat, Matrix<double> y, int m, Matrix<double> aL1, Matrix<double> wL)
+{
+    Matrix<double> aL2 = yHat;
+    Matrix<double> dC_dZL = (1 / (double)m) * (aL2 - y);
+    Matrix<double> dZL_dWL = aL1;
+    Matrix<double> dC_dWL = dC_dZL * dZL_dWL.Transpose();
+    Matrix<double> dC_dbL = CalculateBiasC(dC_dZL);
+    Matrix<double> dZL_dAL1 = wL;
+    Matrix<double> dC_dAL1 = wL.Transpose() * dC_dZL;
+    return (dC_dWL, dC_dbL, dC_dAL1);
+}
+
+(Matrix<double> dC_dWL, Matrix<double> dC_dL2, Matrix<double> dC_dAL1) BackpropHiddenLayer(Matrix<double> propagator_dC_dAL2, Matrix<double> aL1, Matrix<double> aL2, Matrix<double> wL)
+{
+    Matrix<double> dAL2_dZL = aL2.PointwiseMultiply(1 - aL2);
+    Matrix<double> dC_dZL = propagator_dC_dAL2.PointwiseMultiply(dAL2_dZL);
+    Matrix<double> dZL_dWL = aL1;
+    Matrix<double> dC_dWL = dC_dZL * dZL_dWL.Transpose();
+    Matrix<double> dC_dbL = CalculateBiasC(dC_dWL);
+    Matrix<double> dZL_dAL1 = wL;
+    Matrix<double> dC_dAL1 = dZL_dAL1.Transpose() * dC_dZL;
+    return (dC_dWL, dC_dbL, dC_dAL1);
 }
 
 (Matrix<double> dC_dW1, Matrix<double> dC_db1) BackpropLayer1(Matrix<double> propagator_dC_dA1, Matrix<double> a1, Matrix<double> a0, Matrix<double> w1)
@@ -99,25 +137,6 @@ List<double> Train()
     Matrix<double> dC_dW1 = dC_dZ1 * dZ1_dW1.Transpose();
     Matrix<double> dC_db1 = CalculateBiasC(dC_dW1);
     return (dC_dW1, dC_db1);
-}
-
-(Matrix<double> yHat, Cache cache) FeedForward(Matrix<double> a0)
-{
-    Cache cache = new Cache();
-    cache.a0 = a0;
-    Matrix<double> z1 = w1 * a0;
-    z1 += Broadcast(b1Def, z1.ColumnCount);
-    Matrix<double> a1 = Sigmoid(z1);
-    cache.a1 = a1;
-    Matrix<double> z2 = w2 * a1;
-    z2 += Broadcast(b2Def, z2.ColumnCount);
-    Matrix<double> a2 = Sigmoid(z2);
-    cache.a2 = a2;
-    Matrix<double> z3 = w3 * a2;
-    z3 += Broadcast(b3Def, z3.ColumnCount);
-    Matrix<double> a3 = z3;
-    Matrix<double> yHat = a3;
-    return (yHat, cache);
 }
 
 static Matrix<double> CalculateBiasC(Matrix<double> dC_dZ)
@@ -199,7 +218,5 @@ void WriteMatrix<T>(Matrix<T> matrix) where T : struct, IEquatable<T>, IFormatta
 
 public class Cache
 {
-    public Matrix<double> a0 { get; set; }
-    public Matrix<double> a1 { get; set; }
-    public Matrix<double> a2 { get; set; }
+    public List<Matrix<double>> aVals = new List<Matrix<double>>();
 }
